f981225f6b323a097b84940b92ec47e01c92f18f
qujianyang <59550766+qujianyang@users.noreply.github.com>
2025-12-03 15:18:02 +0800

    Revert "scrapper updated"
    This reverts commit 8922ec6f01fd15797c0f2d595258f1e69867ec6f.

--- pdf_ingestion_system/web_scraper/parsers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Parsers for extracting course data from HTML
-"""
-
-from .catalog_parser import CatalogParser
-from .course_parser import CourseParser
-
-__all__ = ["CatalogParser", "CourseParser"]
